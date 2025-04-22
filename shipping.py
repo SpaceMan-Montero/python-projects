90c88f3eaab51850d597cade75fa9f2a6f6f9897
SpaceMan-Montero <85808256+SpaceMan-Montero@users.noreply.github.com>
2025-04-22 18:14:33 -0400

    Create shipping.py

---
+++ shipping.py
@@ -0,0 +1,27 @@
+weight = 41.5
+
+# Ground Shipping
+if weight <= 2:
+    cost_ground = 1.50*weight + 20
+elif 2 < weight <= 6:
+    cost_ground = 3.00*weight + 20
+elif 6 < weight <= 10:
+    cost_ground = 4.00*weight + 20
+else:
+    cost_ground = 4.75*weight + 20
+print("Ground Shipping $", cost_ground)
+
+# Ground Shipping Premium
+cost_ground_premium = 125.00
+print("Ground Shipping Premium $", cost_ground_premium)
+
+# Drone Shipping
+if weight <= 2:
+    cost_drone = 4.50*weight
+elif 2 < weight <= 6:
+    cost_drone = 9.00*weight
+elif 6 < weight <= 10:
+    cost_drone = 12.00*weight
+else:
+    cost_drone = 14.25*weight
+print("Drone Shipping $", round(cost_drone, 3))
